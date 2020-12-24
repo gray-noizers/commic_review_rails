809307a4b9e31728b27499301bb51c23c6959cc7
gray-noizers <hankun970104@gmail.com>
2020-12-24 22:38:36 +0900

    リセットCSS導入

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,14 @@
 class ReviewsController < ApplicationController
     def index
+        @review = Review.all
     end
 
     def new
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.new(review_params)
+        redirect_to root_path
     end
 end
