6da05a194d48bf45aa93e3f6cb5d54da2f0b2d24
gray-noizers <hankun970104@gmail.com>
2020-12-29 23:26:54 +0900

    新規登録を行えるように設定

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2020_12_27_080604) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
